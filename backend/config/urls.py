ea7a85528580f95d21ebf536e72c5db994b3779f
Kwiatuh <46192203+kwiats@users.noreply.github.com>
2023-01-26 17:16:25 +0100

    Merge radtheone/job-schedule to job-schedule/main (#5)
    * first commit
    
    * add confs
    
    Co-authored-by: radthenone <120286486+radthenone@users.noreply.github.com>
    Co-authored-by: radthenone <krzyzak.codes@gmail.com>

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("debug/", include("debug_toolbar.urls")),
+    ]
