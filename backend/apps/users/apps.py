af41208360a837cf8354bf9c8b0a225d3c79c5c8
Kwiatuh <46192203+kwiats@users.noreply.github.com>
2023-01-28 18:41:44 +0100

    Created apps file and connect them to backend (#9)
    * Created apps file and connected to backend
    
    * Connected to backend

---
+++ backend/apps/users/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):  # type: ignore
+    name = "apps.users"
+    verbose_name = "Employer Profile"
---
+++ backend/apps/workstations/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WorkstationsConfig(AppConfig):  # type: ignore
+    name = "apps.workstations"
+    verbose_name = "Workstations"
