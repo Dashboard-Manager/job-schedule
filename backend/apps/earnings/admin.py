9bef59f51603d776f170ae7eb6a732dc533b1c24
kwiats <pawuskwiatkowski@gmail.com>
2023-01-29 11:27:26 +0100

    Created admin panel for earning model

--- backend/apps/earnings/admin.py
+++ backend/apps/earnings/admin.py
@@ -1,22 +1,23 @@
 from apps.earnings.models import Earnings
+from apps.users.models import Profile
 from django.contrib import admin
+
+
+class ProfileInLine(admin.TabularInline):  # type: ignore
+    model = Profile
 
 
 @admin.register(Earnings)
 class EarningsAdmin(admin.ModelAdmin):  # type: ignore
+
     list_display = [
         "user",
+        "age",
         "brutto_salary",
         "netto_salary",
-        "pension_contribution",
-        "disability_contribution",
-        "sickness_contribution",
-        "ZUS_contributions",
-        "health_care_contribution",
-        "income",
-        "income_tax",
     ]
     readonly_fields = [
+        "age",
         "brutto_salary",
         "netto_salary",
         "pension_contribution",
@@ -27,3 +28,60 @@ class EarningsAdmin(admin.ModelAdmin):  # type: ignore
         "income",
         "income_tax",
     ]
+    fieldsets = (
+        (
+            "Profile informations",
+            {
+                "fields": (
+                    "user",
+                    "age",
+                )
+            },
+        ),
+        (
+            "Salary informations",
+            {
+                "classes": (
+                    "wide",
+                    "extrapretty",
+                ),
+                "fields": (
+                    (
+                        "brutto_salary",
+                        "netto_salary",
+                    ),
+                ),
+            },
+        ),
+        (
+            "Earnings informations",
+            {
+                "classes": (
+                    "wide",
+                    "extrapretty",
+                ),
+                "fields": (
+                    (
+                        "pension_contribution",
+                        "disability_contribution",
+                        "sickness_contribution",
+                    ),
+                    ("ZUS_contributions",),
+                    "health_care_contribution",
+                    (
+                        "income",
+                        "income_tax",
+                    ),
+                ),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("user"),
+            },
+        ),
+    )
