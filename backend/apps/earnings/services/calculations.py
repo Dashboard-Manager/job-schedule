bf74c3d9aee175b7e3fdd33898f6124d3c80fa42
kwiats <pawuskwiatkowski@gmail.com>
2023-01-29 19:43:14 +0100

    Move constants to services, some changes in calculations

--- backend/apps/earnings/services/calculations.py
+++ backend/apps/earnings/services/calculations.py
@@ -1,21 +1,16 @@
-from apps.earnings.constants import DISABILITY, HEALTH_CARE, PENSION, PIT, SICKNESS
-
-
-def calc_pension_contr(salary: float, pension: float = PENSION) -> float:
+def calc_pension_contr(salary: float, pension: float) -> float:
     return round(salary * (pension / 100), 2)
 
 
-def calc_disability_contr(salary: float, disability: float = DISABILITY) -> float:
+def calc_disability_contr(salary: float, disability: float) -> float:
     return round(salary * (disability / 100), 2)
 
 
-def calc_sickness_contr(salary: float, sickness: float = SICKNESS) -> float:
+def calc_sickness_contr(salary: float, sickness: float) -> float:
     return round(salary * (sickness / 100), 2)
 
 
-def calc_health_care_contr(
-    salary: float, zus: float, health_care: float = HEALTH_CARE
-) -> float:
+def calc_health_care_contr(salary: float, zus: float, health_care: float) -> float:
     return round((salary - zus) * (health_care / 100), 2)
 
 
@@ -23,5 +18,5 @@ def calc_income(salary: float, zus: float) -> float:
     return round(salary - zus - 250, 2)
 
 
-def calc_income_tax(income: float, pit: int = PIT) -> int:
+def calc_income_tax(income: float, pit: int) -> int:
     return int(income * (pit / 100) - 300)
