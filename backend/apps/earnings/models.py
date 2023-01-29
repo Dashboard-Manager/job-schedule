ba8bfaa1f9f0e748417b2f60b4e7fa45bb0e13fd
kwiats <pawuskwiatkowski@gmail.com>
2023-01-29 11:34:19 +0100

    Created all properties for earning model

--- backend/apps/earnings/models.py
+++ backend/apps/earnings/models.py
@@ -1,6 +1,4 @@
-from django.db import models
-from django.utils import timezone
-from services.calculations import (
+from apps.earnings.services.calculations import (
     calc_disability_contr,
     calc_health_care_contr,
     calc_income,
@@ -8,9 +6,12 @@ from services.calculations import (
     calc_pension_contr,
     calc_sickness_contr,
 )
+from apps.users.models import Profile
+from django.db import models
+from django.utils import timezone
 
 
-class BaseModel(models.Model):
+class BaseModel(models.Model):  # type: ignore
     class Meta:
         abstract = True
 
@@ -19,53 +20,59 @@ class BaseModel(models.Model):
 
 
 class Earnings(BaseModel):
-    user = models.ForeignKey("Profile")
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    @property
+    def age(self) -> int:
+        return int(self.user.age)
 
     @property
     def brutto_salary(self) -> float:
-        return self.user.salary
+        return float(self.user.salary)
 
     @property
     def pension_contribution(self) -> float:
-        return calc_pension_contr(self.brutto_salary)
+        return float(calc_pension_contr(self.brutto_salary))
 
     @property
     def disability_contribution(self) -> float:
-        return calc_disability_contr(self.brutto_salary)
+        return float(calc_disability_contr(self.brutto_salary))
 
     @property
     def sickness_contribution(self) -> float:
-        return calc_sickness_contr(self.brutto_salary)
+        return float(calc_sickness_contr(self.brutto_salary))
 
     @property
     def ZUS_contributions(self) -> float:
-        return (
-            self.pension_contribution
-            + self.disability_contribution
-            + self.sickness_contribution
+        return float(
+            (
+                self.pension_contribution
+                + self.disability_contribution
+                + self.sickness_contribution
+            )
         )
 
     @property
     def health_care_contribution(self) -> float:
-        return calc_health_care_contr(self.brutto_salary, self.ZUS_contributions)
+        return float(calc_health_care_contr(self.brutto_salary, self.ZUS_contributions))
 
     @property
     def income(self) -> float:
-        return calc_income(self.brutto_salary, self.ZUS_contributions)
+        return float(calc_income(self.brutto_salary, self.ZUS_contributions))
 
     @property
     def income_tax(self) -> int:
-        return calc_income_tax(self.income)
+        if self.user.age > 26:
+            return int(calc_income_tax(self.income))
+        return 0
 
     @property
     def netto_salary(self) -> float:
-        if self.user.age > 26:
-            return (
+        return float(
+            (
                 self.brutto_salary
                 - self.ZUS_contributions
                 - self.health_care_contribution
                 - self.income_tax
             )
-        return (
-            self.brutto_salary - self.ZUS_contributions - self.health_care_contribution
         )
