9bffe90604358c7e483e043b13acf140f5d401ff
kwiats <pawuskwiatkowski@gmail.com>
2023-01-29 09:12:17 +0100

    Added function for round return floats

--- backend/apps/earnings/services/calculations.py
+++ backend/apps/earnings/services/calculations.py
@@ -2,25 +2,25 @@ from apps.earnings.constants import DISABILITY, HEALTH_CARE, PENSION, PIT, SICKN
 
 
 def calc_pension_contr(salary: float, pension: float = PENSION) -> float:
-    return salary * (pension / 100)
+    return round(salary * (pension / 100), 2)
 
 
 def calc_disability_contr(salary: float, disability: float = DISABILITY) -> float:
-    return salary * (disability / 100)
+    return round(salary * (disability / 100), 2)
 
 
 def calc_sickness_contr(salary: float, sickness: float = SICKNESS) -> float:
-    return salary * (sickness / 100)
+    return round(salary * (sickness / 100), 2)
 
 
 def calc_health_care_contr(
     salary: float, zus: float, health_care: float = HEALTH_CARE
 ) -> float:
-    return (salary - zus) * (health_care / 100)
+    return round((salary - zus) * (health_care / 100), 2)
 
 
 def calc_income(salary: float, zus: float) -> float:
-    return salary - zus - 250
+    return round(salary - zus - 250, 2)
 
 
 def calc_income_tax(income: float, pit: int = PIT) -> int:
