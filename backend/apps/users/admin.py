d6aa24911b8fd1105821f9f22845c295084b4cac
kwiats <pawuskwiatkowski@gmail.com>
2023-01-29 11:36:47 +0100

    Created admin panel for profile model

---
+++ backend/apps/users/admin.py
@@ -0,0 +1,36 @@
+from apps.users.models import Profile
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+
+@admin.register(Profile)
+class UserAdmin(admin.ModelAdmin):  # type: ignore
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            _("Personal info"),
+            {"fields": ("first_name", "last_name", "email", "birth_date", "salary")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
