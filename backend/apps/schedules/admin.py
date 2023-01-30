7e03b8457d0520ec286150424c5b1b3a8d56f127
kwiats <pawuskwiatkowski@gmail.com>
2023-01-30 19:32:27 +0100

    Added some features in job admin panel

--- backend/apps/schedules/admin.py
+++ backend/apps/schedules/admin.py
@@ -2,4 +2,39 @@ from apps.schedules.models import Event, Job
 from django.contrib import admin
 
 admin.site.register(Event)
-admin.site.register(Job)
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):  # type: ignore
+    list_display = [
+        "user",
+        "date",
+        "job_hours",
+    ]
+    readonly_fields = [
+        "job_hours",
+    ]
+
+    fieldsets = (
+        (
+            "Employer informations",
+            {
+                "fields": (
+                    "user",
+                    "job_hours",
+                    "date",
+                )
+            },
+        ),
+        (
+            "Working hours",
+            {
+                "fields": (
+                    (
+                        "start_job",
+                        "end_job",
+                    ),
+                )
+            },
+        ),
+    )
