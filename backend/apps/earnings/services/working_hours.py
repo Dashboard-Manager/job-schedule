44691b2e2cdd5dbf93e7408dfbb564000b59aaa6
kwiats <pawuskwiatkowski@gmail.com>
2023-02-02 18:57:31 +0100

    Improve function get_working_hours to calculate extra_job_hours

--- backend/apps/earnings/services/working_hours.py
+++ backend/apps/earnings/services/working_hours.py
@@ -1,15 +1,20 @@
-# TODO: Create function to return quantity of hours from current/last month, year and week from job model for specific user
 from apps.schedules.models import Job
 from apps.users.models import Profile
 from django.db import models
 
 
 def get_working_hours(
-    user: Profile, start_date: models.DateField, end_date: models.DateField
+    user: Profile,
+    start_date: models.DateField,
+    end_date: models.DateField,
+    extra_hours: bool = False,
 ) -> int:
+
     working_days = Job.objects.filter(
         date__range=(start_date, end_date),
         user=user,
     )
-    total_hours = sum([job.job_hours for job in working_days])
+    total_hours = sum(
+        [job.extra_job_hours if extra_hours else job.job_hours for job in working_days]
+    )
     return int(total_hours)
