8b0886e05e6a0ccf1af9b9e3e1ebfcdbac710010
kwiats <pawuskwiatkowski@gmail.com>
2023-02-04 16:52:42 +0100

    Created ProfileFactory for tests with faker and factory-boy

---
+++ backend/apps/users/tests/factories.py
@@ -0,0 +1,27 @@
+from apps.users.models import Profile
+from django.contrib.auth.hashers import make_password
+from factory import LazyFunction
+from factory.django import DjangoModelFactory
+from faker import Faker
+from faker.providers import date_time, internet, python
+
+faker = Faker()
+faker.add_provider(internet)
+faker.add_provider(date_time)
+faker.add_provider(python)
+
+
+class ProfileFactory(DjangoModelFactory):
+    class Meta:
+        model = Profile
+
+    username = LazyFunction(lambda: faker.user_name())
+    password = LazyFunction(lambda: make_password(faker.password()))
+    email = LazyFunction(lambda: faker.email())
+    is_staff = False
+    is_superuser = False
+
+    salary = LazyFunction(
+        lambda: faker.pyfloat(right_digits=2, min_value=0, max_value=9_999_999_999)
+    )
+    birth_date = LazyFunction(lambda: faker.date_of_birth(minimum_age=18))
