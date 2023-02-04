12a0ec3a18500c4bf8112aa25eb33349440de6bf
kwiats <pawuskwiatkowski@gmail.com>
2023-02-04 16:51:49 +0100

    Created manager file

---
+++ backend/apps/users/managers.py
@@ -0,0 +1,8 @@
+def create_user(self, email, date_of_birth, password=None):
+    # TODO: create user here
+    ...
+
+
+def create_superuser(self, email, date_of_birth, password=None):
+    # TODO: create superuser here
+    ...
