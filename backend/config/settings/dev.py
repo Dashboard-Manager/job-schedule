a55a172c425de6483da74060a087c56bb754ee12
Kwiatuh <46192203+kwiats@users.noreply.github.com>
2023-02-04 12:21:18 +0100

    Splited settings into base then dev and local (#22)
    * Splited settings into base then dev and local
    
    * Created __init__.py inside migrations folder
    
    * Changed default postgres user

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -1,36 +1,77 @@
+import socket  # noqa
+
 from config.env import env
 from config.settings.base import *  # noqa
-import socket
+
+# Generals
 
 DEBUG = True
 
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default="random_few_signs")
 
-ALLOWED_HOSTS = [env("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", default=["localhost", "0.0.0.0", "127.0.0.1"])
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# Databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": env("POSTGRES_DB"),
-#         "USER": env("POSTGRES_USER"),
-#         "PASSWORD": env("POSTGRES_PASSWORD"),
-#         "HOST": env("POSTGRES_HOST"),
-#         "PORT": env("POSTGRES_PORT"),
-#     }
-# }
-
-
-INSTALLED_APPS += [  # noqa F405
-    "debug_toolbar",
-]
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
-DEBUG_TOOLBAR_CONFIG = {
-    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("POSTGRES_DB", default="postgres_db"),
+        "USER": env("POSTGRES_USER", default="postgres_user"),
+        "PASSWORD": env("POSTGRES_PASSWORD", default="postgres_pass"),
+        "HOST": env("POSTGRES_HOST", default="postgres"),
+        "PORT": env("POSTGRES_PORT", default="5432"),
+    }
+}
+# Cache
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "",
+    }
 }
 
+# Django-debug-toolbar
+
+INSTALLED_APPS += [  # noqa
+    "debug_toolbar",
+]
+
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa
+
+DEBUG_TOOLBAR_CONFIG = {
+    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+    "SHOW_TEMPLATE_CONTEXT": True,
+}
+
+
+# Runserver-plus conf
+RUNSERVERPLUS_SERVER_ADDRESS_PORT = "0.0.0.0:8000"
+
+SHELL_PLUS = "ipython"
+
+SHELL_PLUS_PRINT_SQL = True
+
+NOTEBOOK_ARGUMENTS = [
+    "--ip",
+    "0.0.0.0",
+    "--port",
+    "8000",
+    "--allow-root",
+    "--no-browser",
+]
+
+IPYTHON_ARGUMENTS = [
+    "--ext",
+    "django_extensions.management.notebook_extension",
+    "--debug",
+]
+
+IPYTHON_KERNEL_DISPLAY_NAME = "Django Shell-Plus"
+
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -o nounset
 
 if [ -z "${POSTGRES_USER}" ]; then
-    base_postgres_image_default_user='postgres'
+    base_postgres_image_default_user='postgres_user'
     export POSTGRES_USER="${base_postgres_image_default_user}"
 fi
 export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
