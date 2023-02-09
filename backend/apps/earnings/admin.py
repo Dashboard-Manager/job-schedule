d30c9b8a3c9ae399e62c22c0509b8c923f425379
kwiats <pawuskwiatkowski@gmail.com>
2023-02-09 18:19:34 +0100

    Created admin panels for earnings app models

--- backend/apps/earnings/admin.py
+++ backend/apps/earnings/admin.py
@@ -1,34 +1,7 @@
-from apps.earnings.models import (  # JobHours,
-    Calculations,
-    Constants,
-    Salaries,
-    Settlements,
-)
-from apps.users.models import Profile
+from apps.earnings.models import Calculations, Constants, JobHours, Settlements
 from django.contrib import admin
 
-admin.site.register([Settlements, Calculations, Constants])
-admin.site.register([Settlements, Calculations, Constants])
 
-
-# @admin.register(Constants, Calculations, Salaries, Settlements)
-# class EarningsAdmin(admin.ModelAdmin):  # type: ignore
-#     list_display = [
-#         "Settlements.user",
-#         "Salaries.brutto_salary",
-#         "Salaries.netto_salary",
-#     ]
-#     readonly_fields = [
-#         "brutto_salary",
-#         "netto_salary",
-#         "pension_contribution",
-#         "disability_contribution",
-#         "sickness_contribution",
-#         "ZUS_contributions",
-#         "health_care_contribution",
-#         "income",
-#         "income_tax",
-#     ]
 @admin.register(Settlements)
 class SettlementsAdmin(admin.ModelAdmin):
     list_display = [
@@ -49,142 +22,56 @@ class ConstantsAdmin(admin.ModelAdmin):
     ]
 
 
-class ProfileInLine(admin.TabularInline):  # type: ignore
-    model = Profile
+@admin.register(Calculations)
+class CalculationsAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "brutto_salary",
+        "netto_salary",
+    ]
+    readonly_fields = [
+        "netto_salary",
+    ]
 
 
-admin.site.register([Constants, Calculations, Salaries, Settlements])
+@admin.register(JobHours)
+class JobAdmin(admin.ModelAdmin):  # type: ignore
+    list_display = [
+        "user",
+        "start_date",
+        "end_date",
+        "hours",
+        "extra_hours",
+    ]
 
-# @admin.register(Constants, Calculations, Salaries, Settlements)
-# class EarningsAdmin(admin.ModelAdmin):  # type: ignore
-#     list_display = [
-#         "Settlements.user",
-#         "Salaries.brutto_salary",
-#         "Salaries.netto_salary",
-#     ]
-#     readonly_fields = [
-#         "brutto_salary",
-#         "netto_salary",
-#         "pension_contribution",
-#         "disability_contribution",
-#         "sickness_contribution",
-#         "ZUS_contributions",
-#         "health_care_contribution",
-#         "income",
-#         "income_tax",
-#     ]
-
-#     fieldsets = (
-#         (
-#             "Profile informations",
-#             {
-#                 "fields": (
-#                     "user",
-#                     "age",
-#                 )
-#             },
-#         ),
-#         (
-#             "Salary informations",
-#             {
-#                 "classes": (
-#                     "wide",
-#                     "extrapretty",
-#                 ),
-#                 "fields": (
-#                     (
-#                         "brutto_salary",
-#                         "netto_salary",
-#                     ),
-#                 ),
-#             },
-#         ),
-#         (
-#             "Constants for calculations",
-#             {
-#                 "classes": ("wide",),
-#                 "fields": (
-#                     "constant_pension_contribution",
-#                     "constant_disability_contribution",
-#                     "constant_sickness_contribution",
-#                     "constant_health_care_contribution",
-#                     "constant_PIT",
-#                 ),
-#             },
-#         ),
-#         (
-#             "Earnings informations",
-#             {
-#                 "classes": (
-#                     "wide",
-#                     "extrapretty",
-#                 ),
-#                 "fields": (
-#                     (
-#                         "pension_contribution",
-#                         "disability_contribution",
-#                         "sickness_contribution",
-#                     ),
-#                     ("ZUS_contributions",),
-#                     "health_care_contribution",
-#                     (
-#                         "income",
-#                         "income_tax",
-#                     ),
-#                 ),
-#             },
-#         ),
-#     )
-#     add_fieldsets = ()
-
-# @admin.register(JobHours)
-# class JobAdmin(admin.ModelAdmin):  # type: ignore
-#     list_display = [
-#         "user",
-#         "start_date",
-#         "end_date",
-#         "hours",
-#         "extra_hours",
-#     ]
-
-# @admin.register(JobHours)
-# class JobAdmin(admin.ModelAdmin):  # type: ignore
-#     list_display = [
-#         "user",
-#         "start_date",
-#         "end_date",
-#         "hours",
-#         "extra_hours",
-#     ]
-
-#     readonly_fields = [
-#         "hours",
-#         "extra_hours",
-#     ]
-#     fieldsets = (
-#         (
-#             "Profile informations",
-#             {
-#                 "fields": (
-#                     "user",
-#                     "hours",
-#                     "extra_hours",
-#                 )
-#             },
-#         ),
-#         (
-#             "Find hours between dates..",
-#             {
-#                 "classes": (
-#                     "wide",
-#                     "extrapretty",
-#                 ),
-#                 "fields": (
-#                     (
-#                         "start_date",
-#                         "end_date",
-#                     ),
-#                 ),
-#             },
-#         ),
-#     )
+    readonly_fields = [
+        "hours",
+        "extra_hours",
+    ]
+    fieldsets = (
+        (
+            "Profile informations",
+            {
+                "fields": (
+                    "user",
+                    "hours",
+                    "extra_hours",
+                )
+            },
+        ),
+        (
+            "Find hours between dates..",
+            {
+                "classes": (
+                    "wide",
+                    "extrapretty",
+                ),
+                "fields": (
+                    (
+                        "start_date",
+                        "end_date",
+                    ),
+                ),
+            },
+        ),
+    )
