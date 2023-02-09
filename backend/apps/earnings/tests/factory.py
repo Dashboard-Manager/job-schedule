b69749e9c6f277eba4a2b5ab6dd0486576002987
kwiats <pawuskwiatkowski@gmail.com>
2023-02-09 18:20:15 +0100

    Created fields for factory

--- backend/apps/earnings/tests/factory.py
+++ backend/apps/earnings/tests/factory.py
@@ -12,7 +12,7 @@ faker.add_providers(date_time)
 
 class ConstantsFactory(DjangoModelFactory):
     PIT = LazyFunction(
-        lambda: faker.pydecimal(left_digit=1, right_digit=2, positive=True)
+        lambda: faker.pydecimal(left_digit=2, right_digit=2, positive=True)
     )
     pension_contribution = LazyFunction(
         lambda: faker.pydecimal(left_digit=1, right_digit=2, positive=True)
@@ -28,26 +28,48 @@ class ConstantsFactory(DjangoModelFactory):
     health_care_contribution = LazyFunction(
         lambda: faker.pydecimal(left_digit=1, right_digit=2, positive=True)
     )
+    user = LazyFunction(lambda: SubFactory(ProfileFactory))
 
 
 class CalculationsFactory(DjangoModelFactory):
+    pension_contribution = LazyFunction(
+        lambda: faker.pydecimal(left_digit=1, right_digit=2, positive=True)
+    )
+    disability_contribution = LazyFunction(
+        lambda: faker.pydecimal(left_digit=1, right_digit=2, positive=True)
+    )
+
+    sickness_contribution = LazyFunction(
+        lambda: faker.pydecimal(left_digit=1, right_digit=2, positive=True)
+    )
+
+    health_care_contribution = LazyFunction(
+        lambda: faker.pydecimal(left_digit=1, right_digit=2, positive=True)
+    )
+    income = LazyFunction(
+        lambda: faker.pydecimal(left_digit=1, right_digit=2, positive=True)
+    )
+    income_tax = LazyFunction(
+        lambda: faker.pydecimal(left_digit=1, right_digit=2, positive=True)
+    )
+    netto_salary = LazyFunction(
+        lambda: faker.pydecimal(left_digit=1, right_digit=2, positive=True)
+    )
+
     constants = LazyFunction(lambda: SubFactory(ConstantsFactory))
-
-
-class SalariesFactory(DjangoModelFactory):
-    brutto_salary = LazyFunction(lambda: faker.random_int())
+    hours = LazyFunction(lambda: SubFactory)
 
 
 class SettlementsFactory(DjangoModelFactory):
     user = LazyFunction(lambda: SubFactory(ProfileFactory))
-    constants = LazyFunction(lambda: SubFactory(ConstantsFactory))
+    date = LazyFunction(lambda: timezone.now())
     calculations = LazyFunction(lambda: SubFactory(CalculationsFactory))
-    salary = LazyFunction(lambda: SubFactory(SalariesFactory))
 
 
 class JobHoursFactory(DjangoModelFactory):
     date = LazyFunction(lambda: timezone.now())
     user = LazyFunction(lambda: SubFactory(ProfileFactory))
-    start_job = LazyFunction(lambda: faker.time(pattern="%H:%M:%S"))
-    end_job = LazyFunction(lambda: faker.time(pattern="%H:%M:%S"))
-    hours = LazyFunction(lambda: faker.random_int(min=0, max=24))
+    start_date = LazyFunction(lambda: faker.date())
+    end_date = LazyFunction(lambda: faker.date())
+    hours = LazyFunction(lambda: faker.random_int(min=0))
+    extra_hours = LazyFunction(lambda: faker.random_int(min=0))
