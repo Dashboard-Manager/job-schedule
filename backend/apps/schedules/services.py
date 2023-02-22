fdc219545ab613c6e96c5e68f7a5b0e17aa3db73
kwiats <pawuskwiatkowski@gmail.com>
2023-02-22 19:23:29 +0100

    Tested logic for get_hours

--- backend/apps/schedules/services.py
+++ backend/apps/schedules/services.py
@@ -4,19 +4,19 @@ from typing import Union
 
 def get_hours(start_job: Union[datetime, str], end_job: Union[datetime, str]):
     if isinstance(start_job, str):
-        start_job = datetime.isoformat(start_job)  # type: ignore
+        start_job = datetime.fromisoformat(start_job)
 
     if isinstance(end_job, str):
-        end_job = datetime.isoformat(end_job)  # type: ignore
+        end_job = datetime.fromisoformat(end_job)
 
-    return int(divmod((start_job - end_job).total_seconds(), 3600)[0])  # type: ignore
+    return int(divmod((end_job - start_job).total_seconds(), 3600)[0])  # type: ignore
 
 
 def get_job_hours(start_job: datetime, end_job: datetime):
-    hours = get_hours(end_job, start_job)  # noqa
+    hours = get_hours(end_job=end_job, start_job=start_job)  # noqa
     return min(hours, 8.0)
 
 
 def get_extra_job_hours(start_job: datetime, end_job: datetime):
-    hours = get_hours(end_job, start_job)  # noqa
+    hours = get_hours(end_job=end_job, start_job=start_job)  # noqa
     return max(0, hours - 8)
--- backend/apps/schedules/test/test_models.py
+++ backend/apps/schedules/test/test_models.py
@@ -1,4 +1,7 @@
+import datetime
+
 import pytest
+from apps.schedules.services import get_hours
 from apps.schedules.test.factory import JobFactory
 
 
@@ -22,3 +25,16 @@ class TestJob:
         assert job.extra_hours == max(
             0, divmod((job.start_job - job.end_job).total_seconds(), 3600)[0] - 8
         )
+
+    def test_get_hours(self):
+        start_job_str = "2022-02-22T10:00:00"
+        end_job_str = "2022-02-22T12:30:00"
+        expected_hours = 2
+        # start_job = datetime.datetime.strptime(start_job_str, "%Y-%m-%dT%H:%M:%S")
+        # end_job = datetime.datetime.strptime(end_job_str, "%Y-%m-%dT%H:%M:%S")
+
+        start_job = datetime.datetime.fromisoformat(start_job_str)
+        end_job = datetime.datetime.fromisoformat(end_job_str)
+
+        assert get_hours(start_job=start_job_str, end_job=end_job_str) == expected_hours
+        assert get_hours(start_job, end_job) == expected_hours
