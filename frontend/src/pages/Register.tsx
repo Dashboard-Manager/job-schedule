e6dcf2624eef9bac1f91587488852de58d560596
FuuKowatty <miedzio.2000@gmail.com>
2023-04-15 18:30:25 +0200

    create page

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,9 @@
+export function Register() {
+  return (
+    <>
+      <h1 className='text-3xl tracking-wide text-cyan-800'>siema z rana</h1>
+      <input type='text' className='border-2 border-red-400 p-2' />
+      siema
+    </>
+  )
+}
