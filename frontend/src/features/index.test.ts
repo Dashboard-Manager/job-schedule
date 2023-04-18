0169563877f838cd8ee83cec8e356932da243ad8
FuuKowatty <miedzio.2000@gmail.com>
2023-04-18 22:07:44 +0200

    test

--- frontend/src/features/index.test.ts
+++ frontend/src/features/index.test.ts
@@ -1,5 +1,5 @@
-import { expect, test } from '@jest/globals'
 import sum from './index'
+import { expect, test } from '@jest/globals'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
