ad465b24bf701c018448aed4327ed3dbfd95899f
FuuKowatty <miedzio.2000@gmail.com>
2023-04-18 22:06:41 +0200

    tailwind configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      'black-bg': '#12181b',
+      'gray-dark': '#2a2e35',
+      'gray-light': '#454e56',
+      'white-text': '#fff',
+      'secondary-text': '#88919e',
+      bloody: '#e2243f',
+      success: '#09c372',
+    },
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        user: 'url("https://svgshare.com/i/sAi.svg")',
+      },
+    },
   },
   plugins: [],
 }
