0d7a9cad6cb1c0e2b1e7f82835f20375ebeb7d82
FuuKowatty <miedzio.2000@gmail.com>
2023-04-18 22:04:45 +0200

    basic layout without working validation

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,3 +1,55 @@
+import { Board } from 'components/Board'
+import { Button } from 'components/Button'
+import { DescriptionTypography } from 'components/DescriptionTypography'
+import { FormElement } from 'components/FormElement'
+import { FormElements } from 'utils'
+import { useFormik } from 'formik'
+import { Link } from 'react-router-dom'
+
 export function Login() {
-  return <>Login page</>
+  const { handleBlur, handleChange, handleSubmit, values } = useFormik({
+    initialValues: {
+      name: '',
+      username: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+
+  return (
+    <div className='flex h-[100vh] w-[100vw] items-center justify-center bg-black-bg'>
+      <Board>
+        <h1 className='mb-2 text-center text-4xl font-bold'>Login</h1>
+        <DescriptionTypography>Fill the form and play!</DescriptionTypography>
+        <form onSubmit={handleSubmit}>
+          {FormElements.LoginPage.map((element) => {
+            const valueOfElement = element.name as keyof typeof values
+
+            return (
+              <FormElement
+                key={element.id}
+                type={element.type}
+                name={element.name}
+                icon={element.icon}
+                value={values[valueOfElement]}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            )
+          })}
+          <div className='mb-2 mt-8 text-center'>
+            <Button type='submit'>Login</Button>
+          </div>
+        </form>
+        <DescriptionTypography>
+          Don&apos;t have an account ?{' '}
+          <Link className='text-success' to='/register'>
+            Create an account.
+          </Link>{' '}
+        </DescriptionTypography>
+      </Board>
+    </div>
+  )
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,57 @@
+import { Board } from 'components/Board'
+import { Button } from 'components/Button'
+import { DescriptionTypography } from 'components/DescriptionTypography'
+import { FormElement } from 'components/FormElement'
+import { formSchema } from 'schemas'
+import { FormElements } from 'utils'
+import { useFormik } from 'formik'
+import { Link } from 'react-router-dom'
+
+
 export function Register() {
+  const {errors, handleBlur, handleChange, handleSubmit, values} = useFormik({
+    initialValues: {
+      name: '',
+      username: '',
+      password: '',
+    },
+    validationSchema: formSchema,
+    onSubmit: () => console.log('submitted')
+  })
+
+
   return (
-    <>
-      <h1 className='text-3xl tracking-wide text-cyan-800'>siema z rana</h1>
-      <input type='text' className='border-2 border-red-400 p-2' />
-    </>
+    <div className='flex h-[100vh] w-[100vw] items-center justify-center bg-black-bg'>
+      <Board>
+        <h1 className='mb-2 text-center text-4xl font-bold'>Register</h1>
+        <DescriptionTypography>Fill the form and play!</DescriptionTypography>
+        <form onSubmit={handleSubmit}>
+          {FormElements.RegisterPage.map((element) => {
+            const valueOfElement = element.name as keyof typeof values
+
+            return (
+              <FormElement
+                key={element.id}
+                type={element.type}
+                name={element.name}
+                icon={element.icon}
+                value={values[valueOfElement]}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            )
+          })}
+          <div className='mb-2 mt-8 text-center'>
+            <Button type='submit'>Register</Button>
+          </div>
+        </form>
+        <DescriptionTypography>
+          Already have an account?{' '}
+          <Link className='text-success' to='/login'>
+            Log in.
+          </Link>{' '}
+        </DescriptionTypography>
+      </Board>
+    </div>
   )
 }
