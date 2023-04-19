419f9647c452c4266f4a50fb3574b81936610357
FuuKowatty <miedzio.2000@gmail.com>
2023-04-19 15:44:23 +0200

    fix tailwind colors problem

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,21 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      'black-bg': '#12181b',
-      'gray-dark': '#2a2e35',
-      'gray-light': '#454e56',
-      'white-text': '#fff',
-      'secondary-text': '#88919e',
-      bloody: '#e2243f',
-      success: '#09c372',
-    },
     fontFamily: {
       sans: ['Open Sans', 'sans-serif'],
     },
     extend: {
       backgroundImage: {
         user: 'url("https://svgshare.com/i/sAi.svg")',
+      },
+      colors: {
+        'black-bg': '#12181b',
+        'gray-dark': '#2a2e35',
+        'gray-light': '#454e56',
+        'white-text': '#fff',
+        'secondary-text': '#88919e',
+        bloody: '#e2243f',
+        success: '#09c372',
       },
     },
   },
