857f666e3f106db04e6729196c7ea750ca351ff7
FuuKowatty <miedzio.2000@gmail.com>
2023-04-19 15:29:58 +0200

    add event click
    I added event click to blur() after click button and added style to change background onfocus

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,14 +1,24 @@
-import { ReactNode } from 'react'
+import { MouseEvent, ReactNode } from 'react'
 
-export function Button({
-  children,
-  type = 'button',
-}: {
+type Button = {
   children: ReactNode
   type: 'button' | 'submit' | 'reset' | undefined
-}) {
+}
+
+export function Button({ children, type = 'button' }: Button) {
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    const button = event.target as HTMLButtonElement
+    button.blur()
+  }
+
   return (
-    <button type={type} className='bg-bloody px-8 py-2 font-bold active:translate-y-[1px] '>
+    <button
+      type={type}
+      className={
+        'bg-bloody px-8 py-2 font-bold outline-none  focus:bg-blue-500 active:translate-y-[1px]'
+      }
+      onClick={handleClick}
+    >
       {children}
     </button>
   )
