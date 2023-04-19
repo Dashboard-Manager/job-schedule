e35ab09c2709d5aa4199ed2b4296f69e1c242cf2
FuuKowatty <miedzio.2000@gmail.com>
2023-04-19 15:40:42 +0200

    basic validation of form  register

--- frontend/src/schemas/index.ts
+++ frontend/src/schemas/index.ts
@@ -1,7 +1,5 @@
 import * as yup from 'yup'
 
-// const passwordRules = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+~`\-={}[\]:;"'<>,.?/|]{6,16}$/
-
 const passwordSchema = yup
   .string()
   .required('Password is required')
@@ -9,26 +7,11 @@ const passwordSchema = yup
   .max(16, 'Password cannot be more than 16 characters')
   .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
   .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-  .matches(/\d/, 'Password must contain at least one number');
-
-export function validatePassword(password: string): string[] | undefined {
-  try {
-    passwordSchema.validateSync(password, { abortEarly: false });
-    return undefined;
-  } catch (e) {
-    if (e instanceof yup.ValidationError) {
-      return e.errors;
-    }
-    throw e;
-  }
-}
+  .matches(/\d/, 'Password must contain at least one number')
 
 export const formSchema = yup.object().shape({
-  name: yup.string().required('This field is required'),
+  name: yup.string().min(3).required('This field is required'),
   username: yup.string().min(5).required('This field is required'),
   password: passwordSchema,
-  confirmPassword: yup
-    .string()
-    .required('Password confirmation is required')
-    .oneOf([yup.ref('password')], 'Passwords must match'),
-});
+  confirmPassword: yup.string(),
+})
